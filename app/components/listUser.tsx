42e6e96c1114881e062120116fe84facd6a2b34d
gusthavo.vieira <gusthavorangel@gmail.com>
2023-11-09 22:56:29 -0300

    test crud

---
+++ app/components/listUser.tsx
@@ -0,0 +1,74 @@
+'use client'
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
+import { useState } from "react"
+
+interface User {
+  name: string;
+  email: string;
+  group: string;
+}
+
+export const ListUser = () => {
+  const [users, setUsers] = useState([])
+  const listUsers = async () => {
+    try {
+      const response = await fetch('http://localhost:3333/user/')
+
+      if (!response.ok) {
+        throw new Error('Erro ao realizar a requisição');
+      }
+      const data = await response.json();
+
+      console.log('data', data);
+
+      setUsers(!!data ? data.user : [])
+
+    } catch (error) {
+      console.error('Erro:', error);
+    }
+  }
+
+  if (users?.length <= 0) {
+    listUsers()
+  }
+
+  const onDelete = async (value: any) => {
+    const user_id = value.id;
+    console.log('user_id:', user_id);
+    try {
+      await fetch(`http://localhost:3333/user/${user_id}`, {
+        method: 'DELETE',
+      });
+      alert('Usuário deletado');
+    } catch (error) {
+      alert('Erro ao deletar usuário');
+    }
+  };
+
+
+  return (
+    <TableContainer component={Paper} variant="elevation">
+      <Table size='small' sx={{ ":right": "100" }}>
+        <TableHead>
+          <TableRow>
+            <TableCell color="primary" align="center">Nome</TableCell>
+            <TableCell align="center">E-mail</TableCell>
+            <TableCell align="center">Grupo</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users?.map((item) =>
+            <>
+              <TableRow>
+                <TableCell align="center">{item?.name}</TableCell>
+                <TableCell align="center">{item?.email}</TableCell>
+                <TableCell align="center">{'Pe na trilha'}</TableCell>
+                <TableCell align="center"><Button className="bg-blue-50" onClick={() => onDelete(item)}>Deletar</Button></TableCell>
+              </TableRow>
+            </>
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
---
+++ page.tsx
@@ -0,0 +1,76 @@
+'use client'
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography'
+import { ListUser } from './components/listUser';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import TextField from '@mui/material/TextField';
+import { Grid } from '@mui/material';
+import { CreateUser } from './components/createUser';
+
+export default function Home() {
+  const [value, setValue] = React.useState('1');
+  const [listUser, setListUser] = React.useState(false)
+  const [createUser, setCreateUser] = React.useState(false)
+  const [alterUser, setAlterUSer] = React.useState(false)
+  const [deleteUser, setDeleteUser] = React.useState(false)
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+  const handleListUser = () => {
+    setCreateUser(false)
+    setAlterUSer(false)
+    setDeleteUser(false)
+    setListUser(true)
+  }
+
+  const handleCreateUser = () => {
+    setAlterUSer(false)
+    setDeleteUser(false)
+    setListUser(false)
+    setCreateUser(true)
+  }
+
+  const handleAlterUser = () => {
+    setCreateUser(false)
+    setDeleteUser(false)
+    setListUser(false)
+    setAlterUSer(true)
+  }
+
+  const handleDeleteUser = () => {
+    setCreateUser(false)
+    setAlterUSer(false)
+    setListUser(false)
+    setDeleteUser(true)
+  }
+
+  return (
+    <>
+    <Typography variant="h4">
+      Prototipo Evolutivo
+    </Typography>
+    <br/>
+    <div>
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box>
+          <TabList onChange={handleChange} centered>
+            <Tab label="Listar usuário" value="1" />
+            <Tab label="Cadastrar usuário" value="2" />
+          </TabList>
+        </Box>
+        <TabPanel value="1"><ListUser/></TabPanel>
+        <TabPanel value="2"><CreateUser/></TabPanel>
+      </TabContext>
+    </Box>
+    </div>
+    </>
+  )
+}
