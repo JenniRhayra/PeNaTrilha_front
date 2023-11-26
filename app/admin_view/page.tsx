c601e50682bcc84170c9385c55faa9f417b1e90c
Jennifer Campos <jenniferrhayra@gmail.com>
2023-11-26 00:06:29 -0300

    chore:adc design

--- app/admin_view/page.tsx
+++ app/admin_view/page.tsx
@@ -4,13 +4,14 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography'
-import { ListUser } from './components/listUser';
+import { ListUser } from '../components/listUser';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import TextField from '@mui/material/TextField';
 import { Grid } from '@mui/material';
-import { CreateUser } from './components/createUser';
+import { CreateUser } from '../components/createUser';
+import Image from 'next/image';
 
 export default function Home() {
   const [value, setValue] = React.useState('1');
@@ -52,25 +53,38 @@ export default function Home() {
   }
 
   return (
-    <>
-    <Typography variant="h4">
-      Prototipo Evolutivo
-    </Typography>
-    <br/>
-    <div>
-    <Box sx={{ width: '100%', typography: 'body1' }}>
-      <TabContext value={value}>
-        <Box>
-          <TabList onChange={handleChange} centered>
-            <Tab label="Listar usuário" value="1" />
-            <Tab label="Cadastrar usuário" value="2" />
-          </TabList>
-        </Box>
-        <TabPanel value="1"><ListUser/></TabPanel>
-        <TabPanel value="2"><CreateUser/></TabPanel>
-      </TabContext>
-    </Box>
-    </div>
-    </>
+    <main className="bg-[#F8F8F8]">
+      <div className='absolute sm:w-50 sm:h-50 w-70 h-70 lg:w-100 lg:h-100 top-0 left-0 z-100'>
+        <Image
+            src="/images/img_abs_03.png"
+            alt="forma abstrata"
+            width={300}
+            height={300}
+        />
+      </div>
+      <div className='col-span-12 lg:col-span-5 grid place-items-center mt-10'>
+          <Image
+              src="/images/penatrilha_logo_w_sf.png"
+              alt="logo pe na trilha"
+              width={400}
+              height={400}
+          />
+      </div>
+      <h1 className="text-[#4D5D47] mb-4 text-3xl lg:text-4xl uppercase font-bold text-center">Contas criadas</h1>
+      <h2 className="text-[#C1C1C1] mb-4 text-2xl lg:text-3xl text-center">Alteração e exclusão</h2>
+      <br/>
+      <div>
+      <Box sx={{ width: '100%', typography: 'body1' }}>
+        <TabContext value={value}>
+          <Box>
+            <TabList onChange={handleChange} centered>
+              <Tab label="Lista de usuários" value="1" />
+            </TabList>
+          </Box>
+          <TabPanel value="1"><ListUser/></TabPanel>
+        </TabContext>
+      </Box>
+      </div>
+    </main>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,76 +1,73 @@
-'use client'
+"use client"
 
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography'
-import { ListUser } from './components/listUser';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
-import TextField from '@mui/material/TextField';
-import { Grid } from '@mui/material';
-import { CreateUser } from './components/createUser';
+import { styled } from '@mui/material/styles';
+import Button, { ButtonProps } from '@mui/material/Button';
+import Link from "next/link";
+import Image from 'next/image';
+
+const ColorButton1 = styled(Button)<ButtonProps>(({ theme }) => ({
+    color: theme.palette.getContrastText('#1C2312'),
+    backgroundColor: '#1C2312',
+    '&:hover': {
+        backgroundColor: '#414D33',
+    },
+}));
+
+const ColorButton2 = styled(Button)<ButtonProps>(({ theme }) => ({
+    color: theme.palette.getContrastText('#414D33'),
+    backgroundColor: '#414D33',
+    '&:hover': {
+        backgroundColor: '#7D9662',
+    },
+}));
 
 export default function Home() {
-  const [value, setValue] = React.useState('1');
-  const [listUser, setListUser] = React.useState(false)
-  const [createUser, setCreateUser] = React.useState(false)
-  const [alterUser, setAlterUSer] = React.useState(false)
-  const [deleteUser, setDeleteUser] = React.useState(false)
-
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue);
-  };
-
-  const handleListUser = () => {
-    setCreateUser(false)
-    setAlterUSer(false)
-    setDeleteUser(false)
-    setListUser(true)
-  }
-
-  const handleCreateUser = () => {
-    setAlterUSer(false)
-    setDeleteUser(false)
-    setListUser(false)
-    setCreateUser(true)
-  }
-
-  const handleAlterUser = () => {
-    setCreateUser(false)
-    setDeleteUser(false)
-    setListUser(false)
-    setAlterUSer(true)
-  }
-
-  const handleDeleteUser = () => {
-    setCreateUser(false)
-    setAlterUSer(false)
-    setListUser(false)
-    setDeleteUser(true)
-  }
-
-  return (
-    <>
-    <Typography variant="h4">
-      Prototipo Evolutivo
-    </Typography>
-    <br/>
-    <div>
-    <Box sx={{ width: '100%', typography: 'body1' }}>
-      <TabContext value={value}>
-        <Box>
-          <TabList onChange={handleChange} centered>
-            <Tab label="Listar usuário" value="1" />
-            <Tab label="Cadastrar usuário" value="2" />
-          </TabList>
-        </Box>
-        <TabPanel value="1"><ListUser/></TabPanel>
-        <TabPanel value="2"><CreateUser/></TabPanel>
-      </TabContext>
-    </Box>
-    </div>
-    </>
-  )
+    return (
+        <div className="flex flex-col h-screen min-h-screen bg-[#90A876]">
+            <div className='absolute sm:w-30 sm:h-30 w-50 h-50 lg:w-70 lg:h-70 top-0 left-0 z-100'>
+                <Image
+                    src="/images/img_abs_03.png"
+                    alt="forma abstrata esquerda"
+                    width={200}
+                    height={200}
+                />
+            </div>
+            <div className='col-span-12 lg:col-span-5 grid place-items-center mt-20'>
+                <Image
+                    src="/images/penatrilha_logo_g.png"
+                    alt="logo pe na trilha"
+                    width={500}
+                    height={500}
+                />
+            </div>
+            <div className='col-span-12 lg:col-span-5 grid place-items-center my-1'>
+                <h1 className="text-[#ffffff] text-2xl lg:text-3xl uppercase font-bold text-center mt-5">Vamos começar?</h1>
+            </div>
+            <div className="text-center">
+                <Box
+                    component="form"
+                    sx={{ m: 1 }}
+                    >
+                    <div>
+                        <ColorButton1 sx={{ width: '40ch' }} variant="contained" href="#">FAZER LOGIN</ColorButton1>
+                    </div>
+                    <div>
+                        <p className="text-[#ffffff] my-1">OU</p>
+                        <ColorButton2 sx={{ width: '40ch' }} variant="contained" href="#">COMEÇAR SEM LOGIN</ColorButton2>
+                    </div>
+                </Box>
+            </div>
+            <div className='relative bottom-0'>
+                <Image
+                    src="/images/img_abs_02.png"
+                    alt="forma abstrata direita"
+                    layout="responsive"
+                    width={400}
+                    height={400}
+                />
+            </div>
+        </div>
+    );
 }
