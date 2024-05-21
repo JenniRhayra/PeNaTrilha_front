058992d098c448e52a3243489f9c985a065a7ac8
JuliaCortizo <cortizo.juliax@gmail.com>
2024-05-21 19:04:13 -0300

    modify search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,25 +3,129 @@
 import React from 'react';
 import FooterMenu from '../components/footerMenu';
 import Header from '../components/header';
+import SearchComponent from '../components/searchComponent';
+import {Autocomplete, AutocompleteItem} from "@nextui-org/autocomplete";
+import Image from 'next/image';
+import { useState } from 'react';
+import Card from '../components/cardComponent';
 import LocationComponent from '../components/locationComponent';
-import PageComponentList from '../components/pageComponentList';
+import Grid from "@nextui-org/react"
+
+//Essa info vai vir da consulta do backend
+const parks = [
+  {
+    title: 'Parque Nacional do Iguaçu',
+    image: '/images/parque-iguacu.jpg', // Substitua pelo caminho real da imagem
+    description: 'Famoso pelas Cataratas do Iguaçu, uma das novas sete maravilhas da natureza.',
+    link: 'https://github.com/JenniRhayra/PeNaTrilha_back/branches'
+  },
+  {
+    title: 'Parque Nacional da Chapada dos Veadeiros',
+    image: '/images/parque-carlos-botelho.jpg', // Substitua pelo caminho real da imagem
+    description: 'Conhecido por suas paisagens de cerrado, cachoeiras e trilhas deslumbrantes.',
+    link: 'https://github.com/JenniRhayra/PeNaTrilha_back/branches'
+  },
+  {
+    title: 'Parque Nacional de Jericoacoara',
+    image: '/images/parque-carlos-botelho.jpg', // Substitua pelo caminho real da imagem
+    description: 'Popular por suas dunas, lagoas cristalinas e praias paradisíacas.',
+    link: 'https://github.com/JenniRhayra/PeNaTrilha_back/branches'
+  },
+  {
+    title: 'Parque Nacional do Iguaçu',
+    image: '/images/parque-iguacu.jpg', // Substitua pelo caminho real da imagem
+    description: 'Famoso pelas Cataratas do Iguaçu, uma das novas sete maravilhas da natureza.',
+    link: 'https://github.com/JenniRhayra/PeNaTrilha_back/branches'
+  },
+  {
+    title: 'Parque Nacional do Iguaçu',
+    image: '/images/parque-iguacu.jpg', // Substitua pelo caminho real da imagem
+    description: 'Famoso pelas Cataratas do Iguaçu, uma das novas sete maravilhas da natureza.',
+    link: 'https://github.com/JenniRhayra/PeNaTrilha_back/branches'
+  },
+  {
+    title: 'Parque Nacional do Iguaçu',
+    image: '/images/parque-iguacu.jpg', // Substitua pelo caminho real da imagem
+    description: 'Famoso pelas Cataratas do Iguaçu, uma das novas sete maravilhas da natureza.',
+    link: 'https://github.com/JenniRhayra/PeNaTrilha_back/branches'
+  }
+];
+
+const items = [
+  {
+    key: "parque",
+    label: "Parques",
+  },
+  {
+    key: "guia",
+    label: "Guias",
+  },
+  {
+    key: "trilha",
+    label: "Trilhas",
+  }
+];
 
 const Search: React.FC = () => {
+
+  const [isDivVisible, setIsDivVisible] = useState(false);
+
+  const handleImageClick = () => {
+    setIsDivVisible(!isDivVisible);
+  };
+
   return (
-    <div>
-      <Header />
-      <div className='content'> 
-        <h1 className='text-center'>PÁGINA DE BUSCA</h1>
+    <>
+      <div>
+        <Header></Header>
       </div>
-      <div style={{ margin: '0 auto', padding: '20px',position:'relative'  }}>
-        <LocationComponent />
+      <div style={{ marginTop: '0.5rem', padding: '16px'}}>
+        <LocationComponent></LocationComponent>
+        <SearchComponent title=''></SearchComponent>
+        <br></br>
       </div>
-      <div style={{ margin: '0 auto', padding: '20px', position:'relative', top:'0'}}>
-        <PageComponentList />
+      
+      <div style={{ marginTop: '9rem', padding: '16px'}}>
+          <Image
+             src="/images/filtro.png"
+             alt="filtro"
+             width={25}
+             height={25} 
+             onClick={handleImageClick} 
+          
+          />
+          {isDivVisible && (
+          <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+              <Autocomplete
+                  //label="Favorite Animal"
+                  placeholder="Escolha um filtro"
+                  variant="bordered"
+                  defaultItems={items}
+                  //startContent={<PetIcon className="text-xl" />}
+                  className="max-w-xs"
+                >
+                  {(item) => <AutocompleteItem key={item.key}>{item.label}</AutocompleteItem>}
+              </Autocomplete>
+          </div>
+          )}
+          
+          {parks.map((park, index) => (
+          <div key={index}>
+            <Card
+              title={park.title}
+              image={park.image}
+              description={park.description}
+              link = {park.link}
+            />
+            <br></br>
+          </div>
+        ))}
       </div>
-      <FooterMenu activePage="search" />
-    </div>
+      <FooterMenu activePage='search'></FooterMenu>
+      
+    </>
   );
 };
 
 export default Search;
+
