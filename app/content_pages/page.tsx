2307a6a2b415f7499517788a834140b9e016e311
JENNIFER RHAYRA PIRES DE CAMPO <0030482113007@lti.fatecso.edu.br>
2024-05-21 21:38:20 -0300

    chore:updates page home

--- app/content_pages/page.tsx
+++ app/content_pages/page.tsx
@@ -2,13 +2,18 @@
 import React from 'react';
 import PageHeader from '../components/pageHeaderPromp';
 import FooterMenu from '../components/footerMenu';
+import { FaLocationDot } from "react-icons/fa6";
 
 const ContentPage: React.FC = () => {
   return (
     <div>
       <PageHeader backgroundImageUrl="/images/parque-carlos-botelho.jpg" showCheck={true} title="PE CARLOS BOTELHO">
         {/* Conteúdo dentro do retângulo branco */}
-        <h1>Título da Página</h1>
+        <div>
+          <FaLocationDot size={25} color="black" />
+          <h1>Título da Página</h1>
+        </div>
+        
         <p>Conteúdo da página...</p>
       </PageHeader>
       <FooterMenu activePage="search" />
