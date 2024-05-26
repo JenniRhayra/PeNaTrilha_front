49b23f1b6c4bc7df67d0fa18e42f76c208078a0f
JuliaCortizo <cortizo.juliax@gmail.com>
2024-05-26 17:30:54 -0300

    ajuste responsividade e parametros de filtro

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,14 +1,17 @@
 "use client"
 
-import React from 'react';
+import React, { useContext } from 'react';
 import FooterMenu from '../components/footerMenu';
 import Header from '../components/header';
 import SearchComponent from '../components/searchComponent';
-import {Autocomplete, AutocompleteItem} from "@nextui-org/autocomplete";
+import { Autocomplete, AutocompleteItem } from "@nextui-org/autocomplete";
 import { useState } from 'react';
 import Card from '../components/cardComponent';
 import LocationComponent from '../components/locationComponent';
 import Image from 'next/image';
+
+
+
 
 
 //Essa info vai vir da consulta do backend
@@ -48,101 +51,125 @@ const parks = [
     image: '/images/parque-iguacu.jpg', // Substitua pelo caminho real da imagem
     description: 'Famoso pelas Cataratas do Iguaçu, uma das novas sete maravilhas da natureza.',
     link: 'https://github.com/JenniRhayra/PeNaTrilha_back/branches'
+  },
+  {
+    title: 'Parque Nacional do Iguaçu',
+    image: '/images/parque-iguacu.jpg', // Substitua pelo caminho real da imagem
+    description: 'Famoso pelas Cataratas do Iguaçu, uma das novas sete maravilhas da natureza.',
+    link: 'https://github.com/JenniRhayra/PeNaTrilha_back/branches'
+  },
+  {
+    title: 'Parque Nacional do Iguaçu',
+    image: '/images/parque-iguacu.jpg', // Substitua pelo caminho real da imagem
+    description: 'Famoso pelas Cataratas do Iguaçu, uma das novas sete maravilhas da natureza.',
+    link: 'https://github.com/JenniRhayra/PeNaTrilha_back/branches'
   }
 ];
 
-const items = [
-  {
-    key: "parque",
-    label: "Parques",
-  },
-  {
-    key: "guia",
-    label: "Guias",
-  },
-  {
-    key: "trilha",
-    label: "Trilhas",
-  }
-];
+
 
 const Search: React.FC = () => {
+  const items = [
+    {
+      value: "test",
+      label: "Parques",
+    },
+    {
+      value: "guia",
+      label: "Guias",
+    },
+    {
+      value: "atividade",
+      label: "Atividades",
+    }
+  ];
 
   const [isDivVisible, setIsDivVisible] = useState(false);
+  const [value, setSelectedFilter] = React.useState<React.Key>("parques");
 
   const handleFilterClick = () => {
     setIsDivVisible(!isDivVisible);
   };
 
   const handleSortClick = () => {
-    
+
   };
+
 
   return (
     <>
       <div>
         <Header></Header>
       </div>
-      <div style={{ marginTop: '0.5rem', padding: '16px'}}>
-        <LocationComponent></LocationComponent>
-        <SearchComponent title=''></SearchComponent>
+      <div id='conteinerBusca' style={{ marginTop: '0.5rem', padding: '16px' }}>
+        <SearchComponent title='Parques' term={value ? value.toString() : undefined}></SearchComponent>
         <br></br>
       </div>
-      
-      <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 mt-36 px-4">
+
+      <div className="flex w-full flex-wrap md:flex-wrap mb-6 md:mb-0 gap-4 mt-36 px-4">
         <div className="flex w-full justify-between">
-            <div>
-              <Image id='filtro'
-                src="/images/filtro.png"
-                alt="filtro"
-                width={25}
-                height={25} 
-                onClick={handleFilterClick} 
-              
-              />
-              <label htmlFor="filtro" className='block'> Filtrar </label>
-            </div>
-            
-            <div className="md:self-end">
-              <Image id='ordenar'
-                src="/images/sort.png"
-                alt="ordenar"
-                width={25}
-                height={25} 
-                onClick={handleSortClick} 
-              /> 
-              <label htmlFor="ordenar" className='block'> Ordenar</label>
-            </div>
+          <div>
+            <Image id='filtro'
+              src="/images/filtro.png"
+              alt="filtro"
+              width={25}
+              onClick={handleFilterClick}
+              height={25}
+            />
+            <label htmlFor="filtro" className='block'> Filtrar </label>
+          </div>
+
+          <div className="md:self-end">
+            <Image id='ordenar'
+              src="/images/sort.png"
+              alt="ordenar"
+              width={25}
+              height={25}
+              onClick={handleSortClick}
+            />
+            <label htmlFor="ordenar" className='block'> Ordenar</label>
+          </div>
         </div>
 
-          {isDivVisible && (
-          <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-              <Autocomplete
-                  //label="Favorite Animal"
-                  placeholder="Escolha um filtro"
-                  variant="bordered"
-                  defaultItems={items}
-                  //startContent={<PetIcon className="text-xl" />}
-                  className="max-w-xs"
-                >
-                  {(item: any) => <AutocompleteItem key={item.key}>{item.label}</AutocompleteItem>}
-              </Autocomplete>
+        {isDivVisible && (
+          <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center">
+            <Autocomplete
+              //label="Favorite Animal"
+              placeholder="Escolha um filtro"
+              variant="bordered"
+              defaultItems={items}
+              //startContent={<PetIcon className="text-xl" />}
+              className="max-w-xs"
+              selectedKey={value}
+              onSelectionChange={setSelectedFilter}
+            >
+              {(item) => <AutocompleteItem key={item.value}>{item.label}</AutocompleteItem>}
+            </Autocomplete>
+
+
+            <div id="filtroSelecionado"> Filtro selecionado: {value} </div>
+
+
           </div>
-          )}
-          
+        )}
+        <div id='cardsConteiner' className='w-full flex flex-wrap justify-center'>
           {parks.map((park, index) => (
-          <div key={index}>
-            <Card 
-              title={park.title}
-              image={park.image}
-              description={park.description}
-              link = {park.link}
-            />
-          </div>
-        ))}
+            <div key={index}>
+              <Card
+                title={park.title}
+                image={park.image}
+                description={park.description}
+                link={park.link}
+                distancia={'3.6KM'}
+
+              />
+            </div>
+          ))}
+        </div>
+
       </div>
       <FooterMenu activePage='search'></FooterMenu>
-      
+
     </>
   );
 };
