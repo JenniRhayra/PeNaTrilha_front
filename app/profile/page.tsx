26b4150e4a0ae3738a1e9a967ca8790893bd8a2a
JuliaCortizo <cortizo.juliax@gmail.com>
2024-05-28 21:39:43 -0300

    add icon prfofile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,13 +4,18 @@ import React from 'react';
 import FooterMenu from '../components/footerMenu';
 import Header from '../components/header';
 import '../globals.css';
-
+import { Avatar } from '@nextui-org/react';
 const Profile: React.FC = () => {
   return (
     <div>
         <Header />
-        <div className='content'> 
-            <h1 className='text-center'>Habemos Profile Page</h1>
+        <div className='perfil' style={{paddingTop:'5rem', paddingLeft:'1rem'}}> 
+          <div id='imagem'>
+            <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026708c" className="w-20 h-20 text-large mt-3" />
+          </div>
+          <div id='infoPerfil'>
+            <h1 style={{color:'#4D5D47', fontWeight:'700'}}> MEU PERFIL </h1>
+          </div>
         </div>
         <FooterMenu activePage="profile" />
     </div>
