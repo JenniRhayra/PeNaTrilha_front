feeee1bbd7d1aea118744d56a50d127a77d392b6
JuliaCortizo <cortizo.juliax@gmail.com>
2024-06-05 01:53:52 -0300

    add sort menu and configurations

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -4,108 +4,169 @@ import React, { useContext } from 'react';
 import FooterMenu from '../components/footerMenu';
 import Header from '../components/header';
 import SearchComponent from '../components/searchComponent';
-import { Autocomplete, AutocompleteItem } from "@nextui-org/autocomplete";
+import { Autocomplete, AutocompleteItem} from "@nextui-org/react";
 import { useState } from 'react';
 import Card from '../components/cardComponent';
 import LocationComponent from '../components/locationComponent';
 import Image from 'next/image';
-
-
-
-
+import Stack from '@mui/material/Stack';
+import Slider from '@mui/material/Slider';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 //Essa info vai vir da consulta do backend
-const parks = [
+const parksList = [
   {
-    title: 'Parque Nacional do Iguaçu',
+    title: 'Parque A',
     image: '/images/parque-iguacu.jpg', // Substitua pelo caminho real da imagem
     description: 'Famoso pelas Cataratas do Iguaçu, uma das novas sete maravilhas da natureza.',
     link: 'https://github.com/JenniRhayra/PeNaTrilha_back/branches'
   },
   {
-    title: 'Parque Nacional da Chapada dos Veadeiros',
+    title: 'Parque C',
     image: '/images/parque-carlos-botelho.jpg', // Substitua pelo caminho real da imagem
     description: 'Conhecido por suas paisagens de cerrado, cachoeiras e trilhas deslumbrantes.',
     link: 'https://github.com/JenniRhayra/PeNaTrilha_back/branches'
   },
   {
-    title: 'Parque Nacional de Jericoacoara',
+    title: 'Parque B',
     image: '/images/parque-carlos-botelho.jpg', // Substitua pelo caminho real da imagem
     description: 'Popular por suas dunas, lagoas cristalinas e praias paradisíacas.',
     link: 'https://github.com/JenniRhayra/PeNaTrilha_back/branches'
   },
   {
-    title: 'Parque Nacional do Iguaçu',
+    title: 'Parque D',
     image: '/images/parque-iguacu.jpg', // Substitua pelo caminho real da imagem
     description: 'Famoso pelas Cataratas do Iguaçu, uma das novas sete maravilhas da natureza.',
     link: 'https://github.com/JenniRhayra/PeNaTrilha_back/branches'
   },
   {
-    title: 'Parque Nacional do Iguaçu',
+    title: 'Parque F',
     image: '/images/parque-iguacu.jpg', // Substitua pelo caminho real da imagem
     description: 'Famoso pelas Cataratas do Iguaçu, uma das novas sete maravilhas da natureza.',
     link: 'https://github.com/JenniRhayra/PeNaTrilha_back/branches'
   },
   {
-    title: 'Parque Nacional do Iguaçu',
+    title: 'Parque E',
     image: '/images/parque-iguacu.jpg', // Substitua pelo caminho real da imagem
     description: 'Famoso pelas Cataratas do Iguaçu, uma das novas sete maravilhas da natureza.',
     link: 'https://github.com/JenniRhayra/PeNaTrilha_back/branches'
   },
   {
-    title: 'Parque Nacional do Iguaçu',
+    title: 'Parque H',
     image: '/images/parque-iguacu.jpg', // Substitua pelo caminho real da imagem
     description: 'Famoso pelas Cataratas do Iguaçu, uma das novas sete maravilhas da natureza.',
     link: 'https://github.com/JenniRhayra/PeNaTrilha_back/branches'
   },
   {
-    title: 'Parque Nacional do Iguaçu',
+    title: 'Parque G',
     image: '/images/parque-iguacu.jpg', // Substitua pelo caminho real da imagem
     description: 'Famoso pelas Cataratas do Iguaçu, uma das novas sete maravilhas da natureza.',
     link: 'https://github.com/JenniRhayra/PeNaTrilha_back/branches'
   }
 ];
 
-
-
 const Search: React.FC = () => {
-  const items = [
+
+  const itemsFilterPrincipal = [
     {
-      value: "test",
+      value: "p",
       label: "Parques",
     },
     {
-      value: "guia",
+      value: "g",
       label: "Guias",
     },
     {
-      value: "atividade",
+      value: "a",
       label: "Atividades",
     }
   ];
 
-  const [isDivVisible, setIsDivVisible] = useState(false);
-  const [value, setSelectedFilter] = React.useState<React.Key>("parques");
+  const sortOptions = [
+    'A-Z',
+    'Z-A',
+    'Mais próximos'
+  ];
+ 
+  const [isDivFilterVisible, setDivFilterVisible] = useState(false);
+  const [sliderValue, setSliderValue] = React.useState<number>(5);
+  const [filterValue, setSelectedFilter] = React.useState<React.Key>("p");
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [selectedSortIndex, setSelectedIndex] = React.useState(0);
+  const [parks, setParks] = useState<any[]>(parksList);
+
+  const open = Boolean(anchorEl);
 
   const handleFilterClick = () => {
-    setIsDivVisible(!isDivVisible);
+    setDivFilterVisible(!isDivFilterVisible);
   };
 
-  const handleSortClick = () => {
-
+  const handleSortClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
   };
 
+  const handleOrderOfResults = () => {
+    switch(selectedSortIndex){
+      //A-Z
+      case 0:
+        setParks(sortAlphabetically(parksList));
+        console.log(parks);
+        break;
+      //Z-A
+      case 1:
+        setParks(sortAlphabeticallyDesc(parksList));
+        console.log(parks);
+        break;
+      //Mais próximos
+      case 2:
+        break;
+        default: '';
+    }
+  }
+
+  const sortAlphabetically = (p: any): any => {
+    return parksList.slice().sort((a, b) => a.title.localeCompare(b.title));
+  }
+
+  const sortAlphabeticallyDesc = (p: any): any => {
+    return parksList.slice().sort((a, b) => b.title.localeCompare(a.title));
+  }
+
+  const handleSortItemClick = (
+    event: React.MouseEvent<HTMLElement>,
+    index: number,
+  ) => {
+    setSelectedIndex(index);
+    setAnchorEl(null);
+    handleOrderOfResults();
+  };
+
+  const handleCloseSort = () => {
+    setAnchorEl(null);
+  };
+
+  const handleSliderChange = (event: Event, newValue: number | number[]) => {
+    setSliderValue(newValue as number);
+  };
 
   return (
     <>
       <div>
         <Header></Header>
       </div>
-      <div id='conteinerBusca' style={{ marginTop: '0.5rem', padding: '16px' }}>
-        <SearchComponent title='Parques' term={value ? value.toString() : undefined}></SearchComponent>
+      
+      <div id='conteinerBusca' style={{ marginTop: '3rem', marginLeft: '8rem', alignItems:'center'}}>
+        
+        <SearchComponent title='' filterTerm={filterValue ? filterValue.toString() : undefined}></SearchComponent>
         <br></br>
       </div>
 
+      {filterValue?.toString() == 'p' &&(
+          <div>
+            <LocationComponent showMap={false}></LocationComponent>  
+          </div>  
+      )}
       <div className="flex w-full flex-wrap md:flex-wrap mb-6 md:mb-0 gap-4 mt-36 px-4">
         <div className="flex w-full justify-between">
           <div>
@@ -115,6 +176,7 @@ const Search: React.FC = () => {
               width={25}
               onClick={handleFilterClick}
               height={25}
+              className='cursor-pointer'
             />
             <label htmlFor="filtro" className='block'> Filtrar </label>
           </div>
@@ -125,34 +187,66 @@ const Search: React.FC = () => {
               alt="ordenar"
               width={25}
               height={25}
+              className='cursor-pointer'
               onClick={handleSortClick}
             />
+
             <label htmlFor="ordenar" className='block'> Ordenar</label>
+
+            <Menu
+              id="lock-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleCloseSort}
+              MenuListProps={{
+                'aria-labelledby': 'lock-button',
+                role: 'listbox',
+              }}
+            >
+              {sortOptions.map((option, index) => (
+                <MenuItem
+                  key={option}
+                  selected={index === selectedSortIndex}
+                  onClick={(event) => handleSortItemClick(event, index)}
+                >
+                  {option}
+                </MenuItem>
+              ))}
+            </Menu>
           </div>
         </div>
 
-        {isDivVisible && (
-          <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center">
-            <Autocomplete
-              //label="Favorite Animal"
-              placeholder="Escolha um filtro"
-              variant="bordered"
-              defaultItems={items}
-              //startContent={<PetIcon className="text-xl" />}
-              className="max-w-xs"
-              selectedKey={value}
-              onSelectionChange={setSelectedFilter}
-            >
-              {(item) => <AutocompleteItem key={item.value}>{item.label}</AutocompleteItem>}
-            </Autocomplete>
+        {isDivFilterVisible && (
+          <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-start" style={{ borderBottom: '1px solid #667358', borderRadius: '4px', padding:'1em', justifyContent:'center'}}>
 
-
-            <div id="filtroSelecionado"> Filtro selecionado: {value} </div>
-
-
-          </div>
+              <Autocomplete 
+                isRequired
+                label="Selecione um filtro:"
+                labelPlacement={"outside-left"}
+                variant="bordered"
+                defaultItems={itemsFilterPrincipal}
+                //startContent={<PetIcon className="text-xl" />}
+                className="max-w-xs"
+                selectedKey={filterValue}
+                onSelectionChange={setSelectedFilter}
+                defaultSelectedKey={'p'}
+              >
+                {(item) => <AutocompleteItem key={item.value}>{item.label}</AutocompleteItem>}
+              </Autocomplete>
+            
+              {filterValue?.toString() == 'p' &&(
+                <div id='filtrosParque'> 
+                  <label> Distância: </label>
+                  <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+                    <Slider aria-label="Volume" value={sliderValue} onChange={handleSliderChange} />
+                  </Stack>
+                  
+                </div>
+              )}    
+          </div> 
         )}
-        <div id='cardsConteiner' className='w-full flex flex-wrap justify-center'>
+
+        <div id='cardsConteiner' className='w-full flex flex-wrap justify-center' style={{paddingLeft:'inherit', paddingBottom:'3rem'}}>
           {parks.map((park, index) => (
             <div key={index}>
               <Card
@@ -161,6 +255,7 @@ const Search: React.FC = () => {
                 description={park.description}
                 link={park.link}
                 distancia={'3.6KM'}
+                chipIsVisible = {filterValue?.toString() == 'p' ? true : false}
 
               />
             </div>
