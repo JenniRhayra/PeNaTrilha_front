b548bac537711064a8beb814692007e1e88c8c81
JuliaCortizo <cortizo.juliax@gmail.com>
2024-06-05 01:54:50 -0300

    ajuste width div campo de busca para ficar responsivo

--- app/components/searchComponent.tsx
+++ app/components/searchComponent.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useRouter } from 'next/navigation';
 import { FiSearch } from 'react-icons/fi';
 import { Button } from '@nextui-org/react';
+import ButtonGreen from './buttonGreen';
 
 interface SearchComponentProps {
   title: string, // Adicione a propriedade de título
@@ -29,37 +30,43 @@ const SearchComponent: React.FC<SearchComponentProps> = ({ title, filterTerm }) 
       width: '90%', // Defina a largura desejada da caixa de busca
       maxWidth: '900px', // Largura máxima da caixa de busca
       margin: '0 auto', // Centralizar horizontalmente
-      padding: '0 15px' // Adicionar margem fixa à esquerda e à direita
+      padding: '0 15px', // Adicionar margem fixa à esquerda e à direita
+      display: 'flex'
     }}>
   
       {/* Campo de busca */}
-      <div style={{ position: 'relative', marginTop: '10px'}}>
+      <div id='campoBusca' style={{ position: 'relative', width:'100%'}}>
         <input 
           type="text" 
           placeholder="Buscar parque, guia, trilha..." 
           onChange={handleSearch} 
           style={{ 
             width: '100%', // Largura do campo de busca
-            padding: '5px', // Espaçamento interno
+            padding: '6px', // Espaçamento interno
             border: '1px solid #667358',
             borderRadius: '4px'
+            
+            
         }} />
         <div style={{ position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)' }}>
           <FiSearch />
         </div>
       </div>
-        
-      {/* Título da página */}
-      <h1 style={{ 
-        position: 'absolute', 
-        top: '15vh', 
-        left: '50%', 
-        transform: 'translate(-50%, -50%)',
-        textAlign: 'center',
-        textTransform: 'uppercase', 
-        fontSize: '18px', 
-        fontWeight: 'bold', 
-        color: '#667358'}}>{title}</h1>
+
+      <div id='tituloPagina'>
+        {/* Título da página */}
+        <h1 style={{ 
+          position: 'absolute', 
+          top: '15vh', 
+          left: '50%', 
+          transform: 'translate(-50%, -50%)',
+          textAlign: 'center',
+          textTransform: 'uppercase', 
+          fontSize: '18px', 
+          fontWeight: 'bold', 
+          color: '#667358'}}>{title}</h1>
+      </div>  
+
     </div>
   );
 };
